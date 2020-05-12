3ddbc2151943b3e56277f0cd23409a21268d416f
Travster75 <65250492+Travster75@users.noreply.github.com>
2020-05-12 16:57:42 -0400

    Add comments to main.

--- Main.java
+++ Main.java
@@ -4,10 +4,12 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
 
+        //Create the object
         MorseTree mTree = new MorseTree();
 
         String morse = "....X.X.-..X.-..X---XX.--X---X.-.X.-..X-..X.-.-.-X";
 
+        //Translate the string
         String result = mTree.interpretMorse(morse);
 
         System.out.println(result);
