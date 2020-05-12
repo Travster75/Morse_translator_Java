2c07e7fb4e314e40e89bb72dd4c4aec343cbe1c0
Travster75 <65250492+Travster75@users.noreply.github.com>
2020-05-12 17:00:09 -0400

    Added comments to NODE

--- NODE.java
+++ NODE.java
@@ -1,10 +1,12 @@
 package com.company;
 
+//Class for the nodes for the tree
 public class NODE {
     char letter;
     NODE left;
     NODE right;
 
+    //Default constructor
     public NODE()
     {
         letter = ' ';
