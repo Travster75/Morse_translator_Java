f4613d43404c2f5fc239e96e1cd1759e67ba2c39
Travster75 <65250492+Travster75@users.noreply.github.com>
2020-05-12 16:59:44 -0400

    Added comments to MorseTree

--- MorseTree.java
+++ MorseTree.java
@@ -3,25 +3,31 @@ package com.company;
 import java.io.File;
 import java.util.Scanner;
 
+//Class for the Tree
 public class MorseTree {
     private NODE root;
 
+    // Default constructor
     public MorseTree() throws Exception
     {
         root = new NODE();
         root = createTree();
     }
+
+    // Creates the tree
     public NODE createTree() throws Exception
     {
 
         String morseSymbols = "";
         char morseLetter = ' ';
 
-        File inputFile = new File("morse.txt");
+        //Open the morse file
+        File inputFile = new File("//Users/slbender/Desktop/Travis/JavaRefresh/src/com/company/morse.txt");
         Scanner scnr = new Scanner(inputFile);
 
         String temp;
 
+        //Gather the data from the file
         while(scnr.hasNextLine())
         {
             temp = scnr.next();
@@ -29,27 +35,32 @@ public class MorseTree {
             morseSymbols = scnr.nextLine();
             morseSymbols = morseSymbols.trim();
 
+            //Insert into the tree
             root = insertNode(root, morseLetter, morseSymbols);
         }
         return root;
 
     }
+
+    //Inserts data into the tree
     public NODE insertNode(NODE temp, char morseLetter, String symbols)
     {
+        // If reached end of morse sequence
         if (symbols.isEmpty())
         {
             temp.letter = morseLetter;
             return temp;
         }
+        // If dot, go left
         else if (symbols.charAt(0) == '.')
         {
-            if (temp.left == null)
-            {
+            if (temp.left == null) {
                 temp.left = new NODE();
             }
             temp.left = insertNode(temp.left, morseLetter, symbols.substring(1));
             return temp;
         }
+        //If dash, go right
         else if (symbols.charAt(0) == '-')
         {
             if (temp.right == null)
@@ -59,6 +70,7 @@ public class MorseTree {
             temp.right = insertNode(temp.right, morseLetter, symbols.substring(1));
             return temp;
         }
+        //If an error is encountered
         else
         {
             System.out.println("Error");
@@ -66,30 +78,38 @@ public class MorseTree {
         }
 
     }
+
+    //Translates the morse sequence
     public String translate(NODE temp, String morseSymbols, String result)
     {
+        //If reached end of morse sequence
         if (morseSymbols.isEmpty())
         {
             if(temp != null)
             {
+                //Add translated string to result
                 result = result.concat(Character.toString(temp.letter));
             }
             return result;
         }
+        //If dot, go left
         else if (morseSymbols.charAt(0) == '.')
         {
             return translate(temp.left, morseSymbols.substring(1), result);
         }
+        //If dash, go right
         else if (morseSymbols.charAt(0) == '-')
         {
             return translate(temp.right, morseSymbols.substring(1), result);
         }
+        //X represents end of morse letter
         else if (morseSymbols.charAt(0) == 'X')
         {
             if (temp != null && temp.letter != ' ')
             {
                 result = result.concat(Character.toString(temp.letter));
             }
+            //Two X's represents a space
             if (morseSymbols.length() > 1)
             {
                 if (morseSymbols.charAt(1) == 'X')
@@ -97,8 +117,10 @@ public class MorseTree {
                     result = result.concat(" ");
                 }
             }
+            //Begin at top of tree
             return translate(root, morseSymbols.substring(1), result);
         }
+        //Insert unknown characters
         else
         {
             if (temp != null && temp.letter != ' ')
@@ -106,9 +128,11 @@ public class MorseTree {
                 result = result.concat(Character.toString(temp.letter));
             }
             result = result.concat("?");
+            //Begin at top of tree
             return translate(root, morseSymbols.substring(1), result);
         }
     }
+    //Function to handle recursive translate function
     public String interpretMorse(String morseSymbols)
     {
         String result = "";
